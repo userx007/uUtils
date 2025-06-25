42ca78c7f758b9de4ad6a02b05c65074814d9dc2
userx007 <vmpxxl@gmail.com>
2025-06-25 14:18:52 +0200

    Add a simple argsparser
    Signed-off-by: userx007 <vmpxxl@gmail.com>

---
+++ sources/testapps/test_argsparser/src/test_uArgsParser.cpp
@@ -0,0 +1,27 @@
+#include "uArgsParser.hpp"
+
+int main(int argc, char* argv[]) {
+    CommandLineParser cli("My Tool");
+
+    cli.add_option("file", "Input file path");
+    cli.add_option("mode", "Execution mode");
+    cli.add_option("debug", "Enable debug mode");
+
+    cli.parse(argc, argv);
+
+    if (cli.has("debug")) {
+        std::cout << "Debugging enabled\n";
+    }
+
+    if (auto file = cli.get("file")) {
+        std::cout << "File: " << *file << "\n";
+    }
+
+    if (auto mode = cli.get("mode")) {
+        std::cout << "mode: " << *mode << "\n";
+    }
+
+    cli.print_usage();
+
+    return 0;
+}
---
+++ sources/uUtils/inc/uArgsParser.hpp
@@ -0,0 +1,56 @@
+#ifndef UARGS_PARSER_HPP
+#define UARGS_PARSER_HPP
+
+#include <string>
+#include <unordered_map>
+#include <optional>
+#include <iostream>
+
+class CommandLineParser {
+public:
+    CommandLineParser(std::string description = "")
+        : description_(std::move(description)) {}
+
+    void add_option(const std::string& key, const std::string& help_text = "") {
+        help_texts_[key] = help_text;
+    }
+
+    void parse(int argc, char* argv[]) {
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+            if (arg.rfind("--", 0) == 0) {
+                std::string key = arg.substr(2);
+                std::string value;
+                if (i + 1 < argc && argv[i + 1][0] != '-') {
+                    value = argv[++i];
+                }
+                parsed_options_[key] = value;
+            }
+        }
+    }
+
+    std::optional<std::string> get(const std::string& key) const {
+        auto it = parsed_options_.find(key);
+        if (it != parsed_options_.end() && !it->second.empty())
+            return it->second;
+        return std::nullopt;
+    }
+
+    bool has(const std::string& key) const {
+        return parsed_options_.count(key) > 0;
+    }
+
+    void print_usage() const {
+        std::cout << "Usage:\n";
+        for (const auto& [key, text] : help_texts_) {
+            std::cout << "  --" << key << "\t" << text << "\n";
+        }
+    }
+
+private:
+    std::string description_;
+    std::unordered_map<std::string, std::string> help_texts_;
+    std::unordered_map<std::string, std::string> parsed_options_;
+};
+
+#endif // UARGS_PARSER_HPP
